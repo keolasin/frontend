216034784fbe189639aec019f99b1740ab2b02a9
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-03-12 09:48:59 -0700

    add first test, success

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -4,11 +4,14 @@ import { render } from "@testing-library/react"
 import Header from "../header"
 
 const Title = () => {
-  <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+  return (
+    <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+  )
+  
 }
 
 test("Displays the correct title", () => {
-  const { getTestById } = render(<Title />)
+  const { getByTestId } = render(<Title />)
   // Assertion
   expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!");
 })
