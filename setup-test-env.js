1a320963f97664397d059cf8d952aa05e077dcb1
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-03-12 09:00:07 -0700

    add react-testing-library

--- setup-test-env.js
+++ setup-test-env.js
@@ -1,4 +1,5 @@
 import { createSerializer } from "jest-emotion"
+import "@testing-library/jest-dom/extend-expect"
 import * as emotion from "@emotion/core"
 
 expect.addSnapshotSerializer(createSerializer(emotion))
--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -1,13 +1,14 @@
 import React from "react"
-import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
 
 import Header from "../header"
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header siteTitle="Default Starter" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+const Title = () => {
+  <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+}
+
+test("Displays the correct title", () => {
+  const { getTestById } = render(<Title />)
+  // Assertion
+  expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!");
 })
